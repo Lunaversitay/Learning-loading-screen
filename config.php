d994efbc1287a5f08b12c6c53378a5f12b143714
Luna Versity <mrgodzbrother@gmail.com>
2016-03-28 07:34:49 -0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<!-- This is our Configuration file. Here you will configure your loading screen, some would classify it as security for your API key, no idea why but they do -->
+<!-- We're going to start off by using PHP -->
+
+<?php
+
+// Here we are going to add our SteamAPI key, this is essential for everything to sync with steam. No-one can see this but you and people with direct access to your FTP
+// Steam API Key
+$SteamAPIKey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+
+// We need to end our PHP session with this line
+// Now navigate your self to the 'inc' folder then to 'core.php' and we'll continue
+?>
